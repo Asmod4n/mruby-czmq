a30b50a9358bc18b23feae82fe4bd538113d9001
Asmod4n <Asmod4n@users.noreply.github.com>
2015-07-14 09:14:28 +0200

    added tickets to reactor

--- mrblib/reactor.rb
+++ mrblib/reactor.rb
@@ -44,13 +44,42 @@ module CZMQ
       end
     end
 
+    class Ticket
+      attr_accessor :when
+
+      def initialize(reactor, &block)
+        raise ArgumentError, "no block given" unless block_given?
+        @reactor = reactor
+        @delay = @reactor.ticket_delay
+        @when = Zclock.mono + @delay
+      end
+
+      def reset
+        @reactor.reset_ticket(self)
+      end
+
+      def call
+        @block.call(self)
+      end
+    end
+
+    attr_reader :ticket_delay
     attr_accessor :interrupted
 
     def initialize
       @poller = Zpoller.new
       @timers = []
+      @tickets = []
       @readers = {}
+      @ticket_delay = nil
       @interrupted = false
+    end
+
+    def ticket_delay=(delay)
+      raise ArgumentError, "ticket delay cannot be changed" if @ticket_delay
+      delay = Integer(delay)
+      raise ArgumentError, "delay must be 0 or greater" if delay < 0
+      @ticket_delay = delay
     end
 
     def ignore_interrupts
@@ -59,10 +88,10 @@ module CZMQ
     end
 
     def reader(socket, &block)
-      raise ArgumentError, "no block given" unless block_given?
       if @readers.has_key?(socket)
         raise ArgumentError, "each sock can only be polled once"
       end
+      raise ArgumentError, "no block given" unless block_given?
       @poller.add(socket)
       @readers[socket] = block
       self
@@ -85,10 +114,32 @@ module CZMQ
       self
     end
 
+    def ticket(&block)
+      raise ArgumentError, "ticket_delay must be set" unless @ticket_delay
+      ticket = Ticket.new(self, &block)
+      @tickets << ticket
+      ticket
+    end
+
+    def ticket_reset(ticket)
+      ticket = @tickets.delete_at(@tickets.index(ticket))
+      ticket.when = Zclock.mono + @ticket_delay
+      @tickets << ticket
+      ticket
+    end
+
+    def ticket_delete(ticket)
+      @tickets.delete_at(@tickets.index(ticket))
+      self
+    end
+
     def tickless
       tickless = Zclock.mono + 1000
       unless @timers.empty?
         tickless = @timers.min.when
+      end
+      if (ticket = @tickets.first)
+        tickless = ticket.when
       end
       timeout = tickless - Zclock.mono
       timeout < 0 ? 0 : timeout
@@ -103,7 +154,8 @@ module CZMQ
           @readers[socket].call(socket)
         end
         now = Zclock.mono
-        @timers.select {|timer| now >= timer.when}.map(&:call)
+        @timers.select {|timer| now >= timer.when}.each {|timer| timer.call}
+        @tickets.delete_if {|ticket| now >= ticket.when}.each {|ticket| ticket.call}
       end
       self
     end
@@ -116,7 +168,8 @@ module CZMQ
         @readers[socket].call(socket)
       end
       now = Zclock.mono
-      @timers.select {|timer| now >= timer.when}.map(&:call)
+      @timers.select {|timer| now >= timer.when}.each {|timer| timer.call}
+      @tickets.delete_if {|ticket| now >= ticket.when}.each {|ticket| ticket.call}
       self
     end
 
@@ -128,7 +181,8 @@ module CZMQ
         @readers[socket].call(socket)
       end
       now = Zclock.mono
-      @timers.select {|timer| now >= timer.when}.map(&:call)
+      @timers.select {|timer| now >= timer.when}.each {|timer| timer.call}
+      @tickets.delete_if {|ticket| now >= ticket.when}.each {|ticket| ticket.call}
       self
     end
   end
