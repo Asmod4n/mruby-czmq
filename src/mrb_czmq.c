ff49bc88444690a09bcbb88390aded99814f0ca4
Asmod4n <Asmod4n@users.noreply.github.com>
2015-08-16 22:37:54 +0200

    remove mrb_actor_fn

--- src/mrb_czmq.c
+++ src/mrb_czmq.c
@@ -990,13 +990,10 @@ mrb_pollitem_new(mrb_state *mrb, mrb_value self)
       pollitem->socket = mrb_cptr(socket_or_fd);
       break;
     case MRB_TT_DATA: {
-      if (DATA_TYPE(socket_or_fd) == &mrb_zsock_actor_type)
-        pollitem->socket = zsock_resolve(mrb_data_check_get_ptr(mrb, socket_or_fd, &mrb_zsock_actor_type));
-      else
       if (mrb_respond_to(mrb, socket_or_fd, mrb_intern_lit(mrb, "fileno")))
         pollitem->fd = mrb_int(mrb, mrb_funcall(mrb, socket_or_fd, "fileno", 0, NULL));
       else
-        mrb_raise(mrb, E_ARGUMENT_ERROR, "dafuq");
+        pollitem->socket = zsock_resolve(mrb_data_check_get_ptr(mrb, socket_or_fd, &mrb_zsock_actor_type));
     }
       break;
     case MRB_TT_FIXNUM:
@@ -1005,7 +1002,7 @@ mrb_pollitem_new(mrb_state *mrb, mrb_value self)
     case MRB_TT_FALSE:
       break;
     default:
-      mrb_raise(mrb, E_ARGUMENT_ERROR, "dafuq");
+      mrb_raise(mrb, E_ARGUMENT_ERROR, "data type not supported");
   }
 
   mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "socket"), socket_or_fd);
@@ -1102,141 +1099,6 @@ mrb_zmq_poll(mrb_state *mrb, mrb_value self)
   }
 
   return self;
-}
-
-static void
-mrb_zactor_fn(zsock_t *pipe, void *args)
-{
-  const char *mrb_file = (const char *) args;
-  assert(strlen(mrb_file));
-  mrb_state *mrb = mrb_open();
-  assert (mrb);
-
-  FILE *handle = fopen(mrb_file, "r");
-  assert(handle);
-
-  struct mrb_jmpbuf *prev_jmp = mrb->jmp;
-  struct mrb_jmpbuf c_jmp;
-
-  mrb_value global_state, msgpack_mod;
-  mrb_sym global_state_sym, mruby_actor_count;
-
-  MRB_TRY(&c_jmp) {
-    mrb_load_file(mrb, handle);
-    global_state = mrb_hash_new(mrb);
-    global_state_sym = mrb_intern_lit(mrb, "mruby_actor_global_state");
-    mruby_actor_count = mrb_intern_lit(mrb, "count");
-    mrb_gv_set(mrb, global_state_sym, global_state);
-    struct RClass *msgpack_mod_rc = mrb_module_get(mrb, "MessagePack");
-    msgpack_mod = mrb_obj_value(msgpack_mod_rc);
-    zsock_signal(pipe, 0);
-    mrb->jmp = prev_jmp;
-  } MRB_CATCH(&c_jmp) {
-    mrb->jmp = prev_jmp;
-    mrb_p(mrb, mrb_obj_value(mrb->exc));
-    mrb_close(mrb);
-    fclose(handle);
-    zsock_signal(pipe, 1);
-    return;
-  } MRB_END_EXC(&c_jmp);
-
-  bool terminated = false;
-  while(!terminated) {
-    int ai = mrb_gc_arena_save(mrb);
-    zmsg_t *msg = zmsg_recv(pipe);
-    if (!msg)
-      break;    // Interrupted
-
-    char *command = zmsg_popstr (msg);
-    printf("command: %s\n", command);
-    if (streq(command, "$TERM"))
-      terminated = true;
-    else
-    if (streq(command, "initialize")) {
-      struct mrb_jmpbuf *prev_jmp = mrb->jmp;
-      struct mrb_jmpbuf c_jmp;
-      char *class = zmsg_popstr(msg);
-      assert(class);
-      zframe_t *args = zmsg_pop(msg);
-      MRB_TRY(&c_jmp) {
-        struct RClass *mrb_class = mrb_class_get(mrb, class);
-        mrb_value obj;
-        if (args) {
-          mrb_value argv_str = mrb_str_new(mrb, zframe_data(args), zframe_size(args));
-          mrb_value argv = mrb_funcall(mrb, msgpack_mod, "unpack", 1, argv_str);
-          obj = mrb_obj_new(mrb, mrb_class, 1, &argv);
-        } else {
-          obj = mrb_obj_new(mrb, mrb_class, 0, NULL);
-        }
-        mrb_value object_id = mrb_fixnum_value(mrb_obj_id(obj));
-        mrb_value actor_address = mrb_funcall(mrb, object_id, "to_s", 0, NULL);
-        mrb_hash_set(mrb, global_state, actor_address, obj);
-        const char *actor_address_cstr = mrb_string_value_cstr(mrb, &actor_address);
-        zsock_send(pipe, "s", actor_address_cstr);
-        mrb->jmp = prev_jmp;
-      } MRB_CATCH(&c_jmp) {
-        mrb->jmp = prev_jmp;
-        mrb_p(mrb, mrb_obj_value(mrb->exc));
-        terminated = true;
-        zsock_signal(pipe, 1);
-      } MRB_END_EXC(&c_jmp);
-      zstr_free(&class);
-      zframe_destroy(&args);
-    }
-    else
-    if (streq(command, "send")) {
-      zframe_t *object_id_frame = zmsg_pop(msg);
-      assert (object_id_frame);
-      char *func = zmsg_popstr(msg);
-      assert (strlen(func));
-      zframe_t *args = zmsg_pop(msg);
-      struct mrb_jmpbuf *prev_jmp = mrb->jmp;
-      struct mrb_jmpbuf c_jmp;
-      MRB_TRY(&c_jmp) {
-        mrb_value ret;
-        mrb_value actor_address = mrb_str_new_static(mrb, zframe_data(object_id_frame), zframe_size(object_id_frame));
-        mrb_value object_id = mrb_hash_get(mrb, global_state, actor_address);
-        if (args) {
-          mrb_value argv_str = mrb_str_new_static(mrb, zframe_data(args), zframe_size(args));
-          mrb_value argv = mrb_funcall(mrb, msgpack_mod, "unpack", 1, argv_str);
-          mrb_value rets = mrb_funcall(mrb, object_id, func, 1, argv);
-          ret = mrb_funcall(mrb, rets, "to_msgpack", 0, NULL);
-        } else {
-          mrb_value rets = mrb_funcall(mrb, object_id, func, 0, NULL);
-          ret = mrb_funcall(mrb, rets, "to_msgpack", 0, NULL);
-        }
-        if (mrb->exc) {
-          zframe_destroy(&args);
-          zstr_free(&func);
-          zframe_destroy(&object_id_frame);
-          terminated = true;
-          mrb_p(mrb, mrb_obj_value(mrb->exc));
-          zsock_signal(pipe, 1);
-          break;
-        }
-        zframe_t *result = zframe_new(RSTRING_PTR(ret), RSTRING_LEN(ret));
-        zframe_send(&result, pipe, 0);
-        mrb->jmp = prev_jmp;
-      } MRB_CATCH(&c_jmp) {
-        mrb->jmp = prev_jmp;
-        mrb_p(mrb, mrb_obj_value(mrb->exc));
-        terminated = true;
-        zsock_signal(pipe, 1);
-      } MRB_END_EXC(&c_jmp);
-      zframe_destroy(&args);
-      zstr_free(&func);
-      zframe_destroy(&object_id_frame);
-    }
-
-    mrb_gc_arena_restore(mrb, ai);
-    zstr_free(&command);
-    zmsg_destroy(&msg);
-  }
-
-  mrb_gv_remove(mrb, global_state_sym);
-  mrb_close(mrb);
-  fclose(handle);
-  zsock_signal(pipe, 0);
 }
 
 static mrb_value
